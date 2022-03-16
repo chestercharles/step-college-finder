747ae228b834049d57a2f6ab7846f2e69cd07f47
chestercharles <chestercarmer@icloud.com>
2022-03-15 22:20:01 -0700

    add font

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
@@ -7,9 +8,25 @@ import {
   ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import globalStyles from "~/global.css";
 
 export const meta: MetaFunction = () => {
   return { title: "College Finder" };
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Fredoka:wght@300&family=Maven+Pro:wght@600;700;900&family=Open+Sans:wght@300&display=swap",
+    },
+    { rel: "stylesheet", href: globalStyles },
+  ];
 };
 
 export default function App() {
@@ -20,6 +37,10 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        <style>
+          font-family: 'Fredoka', sans-serif; font-family: 'Maven Pro',
+          sans-serif;
+        </style>
       </head>
       <body>
         <Outlet />
